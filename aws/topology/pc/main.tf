86274584badc74a1bfd8093e8f69ae2f6a8fcd59
hyy <ssuzi97@gmail.com>
2025-05-25 16:53:07 +0900

    Update main.tf

--- aws/topology/pc/main.tf
+++ aws/topology/pc/main.tf
@@ -12,6 +12,42 @@ resource "aws_instance" "pc" {
 
 resource "aws_security_group" "pc_sg" {
   name        = "pc-sg"
-  description = "SG for pc"
+  description = "SG for pc with restricted access"
   vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow RDP from admin IP"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["203.0.113.10/32"]
+  }
+
+  ingress {
+    description = "Allow SSH from jump host"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.100/32"]
+  }
+
+  ingress {
+    description = "Allow HTTP from internal network"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.0.0/16"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "pc-sg"
+  }
 }
