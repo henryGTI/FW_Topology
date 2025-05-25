7a47aa1dbf4ef11b870572048a8fd7aecdf6971a
hyy <ssuzi97@gmail.com>
2025-05-25 16:34:45 +0900

    Update variables.tf

---
+++ aws/modules/vpc/variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  description = "VPC 이름"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
