ef18b56c113364b0cc2cf9cc629de8e109bd86a6
hyy <ssuzi97@gmail.com>
2025-05-25 16:36:40 +0900

    Update main.tf

---
+++ aws/topology/network/main.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "topology-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-2a"
+
+  tags = {
+    Name = "topology-subnet"
+  }
+}
