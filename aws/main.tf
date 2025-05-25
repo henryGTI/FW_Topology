0e70a5b53f6692c23f2b70b0f2e851f4704448f1
hyy <ssuzi97@gmail.com>
2025-05-25 16:40:21 +0900

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -8,4 +8,32 @@ module "firewall_1" {
   subnet_id = module.network.subnet_id
 }
 
-...
+module "firewall_2" {
+  source    = "./topology/firewall_2"
+  vpc_id    = module.network.vpc_id
+  subnet_id = module.network.subnet_id
+}
+
+module "server_1" {
+  source    = "./topology/server_1"
+  vpc_id    = module.network.vpc_id
+  subnet_id = module.network.subnet_id
+}
+
+module "server_2" {
+  source    = "./topology/server_2"
+  vpc_id    = module.network.vpc_id
+  subnet_id = module.network.subnet_id
+}
+
+module "server_3" {
+  source    = "./topology/server_3"
+  vpc_id    = module.network.vpc_id
+  subnet_id = module.network.subnet_id
+}
+
+module "pc" {
+  source    = "./topology/pc"
+  vpc_id    = module.network.vpc_id
+  subnet_id = module.network.subnet_id
+}
