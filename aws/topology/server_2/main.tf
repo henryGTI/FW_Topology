a43a97fb49569d5185afd4d6b3406a3f35bbc9de
hyy <ssuzi97@gmail.com>
2025-05-25 16:52:40 +0900

    Update main.tf

--- aws/topology/server_2/main.tf
+++ aws/topology/server_2/main.tf
@@ -12,6 +12,42 @@ resource "aws_instance" "server_2" {
 
 resource "aws_security_group" "server_2_sg" {
   name        = "server_2-sg"
-  description = "SG for server_2"
+  description = "SG for server_2 with restricted access"
   vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow RDP from admin IP"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["203.0.113.10/32"]
+  }
+
+  ingress {
+    description = "Allow SSH from jump host"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.100/32"]
+  }
+
+  ingress {
+    description = "Allow HTTP from internal network"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.0.0/16"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "server_2-sg"
+  }
 }
